782b13f00354c480b7959244b0b3c4dc45e61740
박기현 <kihyun1998@penta.co.kr>
2025-06-30 21:16:38 +0900

    Revert "phase2-1"
    This reverts commit af0f7a1401e836c1d116c8ee347d773d7004a0f3.

--- lib/flutter_basic_table.dart
+++ lib/flutter_basic_table.dart
@@ -1,36 +1,20 @@
 // lib/flutter_basic_table.dart
 library;
 
-/// Core BasicTable widgets (2.x 호환성)
+/// Enums
+export 'src/enum/column_sort_state.dart';
+export 'src/enum/tooltip_position.dart';
+
+/// Core widgets
 export 'src/flutter_basic_table.dart';
 export 'src/models/flutter_basic_table_cell.dart';
+
+/// Models
 export 'src/models/flutter_basic_table_column.dart';
 export 'src/models/flutter_basic_table_row.dart';
 export 'src/models/status_config.dart';
 
-/// SelectableTable (3.x 새로운 API)
-export 'src/selectable/models/selectable_cell.dart';
-export 'src/selectable/models/selectable_column.dart';
-export 'src/selectable/models/selectable_row.dart';
-export 'src/selectable/theme/selectable_border_theme.dart';
-export 'src/selectable/theme/selectable_header_theme.dart';
-export 'src/selectable/theme/selectable_row_theme.dart';
-// export 'src/selectable/theme/selectable_theme.dart';
-// export 'src/selectable/utils/selection_manager.dart';
-// export 'src/selectable/utils/sort_manager.dart';
-export 'src/selectable/widgets/selectable_table.dart';
-
-/// Enums (shared)
-export 'src/shared/enums/column_sort_state.dart';
-export 'src/shared/enums/tooltip_position.dart';
-export 'src/shared/utils/scroll_helper.dart';
-export 'src/shared/utils/theme_utils.dart';
-export 'src/shared/utils/width_calculator.dart';
-
-/// Shared utilities
-export 'src/shared/widgets/custom_inkwell_widget.dart';
-export 'src/shared/widgets/custom_tooltip.dart';
-export 'src/shared/widgets/synced_scroll_controller.dart';
+/// Theme
 export 'src/theme/flutter_basic_table_border_theme.dart';
 export 'src/theme/flutter_basic_table_checkbox_cell_theme.dart';
 export 'src/theme/flutter_basic_table_data_row_theme.dart';
@@ -40,3 +24,6 @@ export 'src/theme/flutter_basic_table_selection_theme.dart';
 export 'src/theme/flutter_basic_table_theme.dart';
 export 'src/theme/flutter_basic_table_tooltip_theme.dart';
 export 'src/utils/column_sort_manager.dart';
+
+/// Utilities
+export 'src/widgets/custom_inkwell_widget.dart';
